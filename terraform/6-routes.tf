ba95c1fd1ff491fca5d525fe0b7237a16fee81f0
iam_mamadou <momodouba44@gmail.com>
2024-09-28 13:18:05 +0000

    Fea: EKS terraform files

--- terraform/6-routes.tf
+++ terraform/6-routes.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "private" {
 
   route  {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat
+    gateway_id = aws_nat_gateway.nat.id
   }
 
   tags = {
@@ -16,7 +16,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags= {
